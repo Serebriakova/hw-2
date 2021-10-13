de8d219ce6246df6fc0483b0fdba0dcb6c781b83
Serebriakova <serebriakova.alla@gmail.com>
2021-10-14 01:17:42 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,14 @@
+const number1 = prompt( "Enter number1");
+const number2 = prompt( "Enter number2");
+
+let sum = Number(number1) + Number(number2);
+console.log(`Result: ${number1} + ${number2} = ${sum}`);
+
+let subtraction = number1 - number2;
+console.log(`Result: ${number1} - ${number2} = ${subtraction}`);
+
+let dividing = number1 / number2;
+console.log(`Result: ${number1} / ${number2} = ${dividing}`);
+
+let multiplication = number1 * number2;
+console.log(`Result: ${number1} * ${number2} = ${multiplication}`);
